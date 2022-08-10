cb049e08c96677d4c2febc259488e482a9d1f5b7
brightGarden02 <rreone2017@gmail.com>
2022-08-10 11:30:58 +0900

    feat: 잭슨으로 객체를 json으로 변경

---
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -0,0 +1,21 @@
+package com.ll.exam.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Ut {
+
+    public static class json {
+
+        public static String toJsonStr(Object obj, String defaultValue){
+
+            ObjectMapper om = new ObjectMapper();
+
+            try {
+                return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+    }
+}
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -5,8 +8,21 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class AppTest {
 
     @Test
-    void 테스트_assertTHat() {
+    void 실험_assertTHat() {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
     }
+
+    @Test
+    void 실험_ObjectMapper() throws JsonProcessingException {
+
+        ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+        assertThat(jsonStr).isNotBlank();
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim());
+    }
+
 }
