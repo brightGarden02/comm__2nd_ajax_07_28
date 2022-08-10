93e3956387e68b2aac838a7f32f4e9c385c873cd
brightGarden02 <rreone2017@gmail.com>
2022-08-10 15:03:15 +0900

    feat: 제너릭을 이용해서 toMap의 기능을 toObj에 통합

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -37,15 +37,8 @@ public class Ut {
             }
         }
 
-        public static <T> List<T> toObj(String jsonStr, TypeReference<List<T>> typeReference, List<T> defaultValue) {
-            try {
-                return om.readValue(jsonStr, typeReference);
-            } catch (JsonProcessingException e) {
-                return defaultValue;
-            }
-        }
 
-        public static <K, V> Map<K, V> toMap(String jsonStr, TypeReference<Map<K, V>> typeReference, Map<K, V> defaultValue) {
+        public static <T> T toObj(String jsonStr, TypeReference<T> typeReference, T defaultValue) {
             try {
                 return om.readValue(jsonStr, typeReference);
             } catch (JsonProcessingException e){
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -97,10 +97,10 @@ public class AppTest {
         articleDtoMap.put("최신", new ArticleDto(2, "제목2", "내용2"));
         String jsonStr = Ut.json.toStr(articleDtoMap, "");
 
-        Map<String, ArticleDto> articleDtoMapFromJson = Ut.json.toMap(jsonStr, new TypeReference<>() {
+        Map<String, ArticleDto> articleDtoMapFromJson = Ut.json.toObj(jsonStr, new TypeReference<>() {
         }, null);
 
-        System.out.println(jsonStr);
+        assertThat(articleDtoMapFromJson).isEqualTo(articleDtoMap);
 
     }
 
